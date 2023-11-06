a6dea5342d0cb4c17321b083bfa2ceb6f14345f9
Kifor Kostiantyn <ks.kifor@gmail.com>
2023-11-06 20:08:43 +0200

    Updated package versions

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,24 @@ const tsNode = require('ts-node');
 const prettier = require('gulp-prettier');
 
 gulp.task('prettify', function () {
-	return gulp.src('.stylelintrc.js')
-		.pipe(prettier({ /* ваші налаштування prettier, якщо вони відмінні від конфігураційного файлу */ }))
-		.pipe(gulp.dest('.'));
+	return gulp.src('.stylelintrc.js').pipe(prettier()).pipe(gulp.dest('.'));
 });
 
-gulp.task('generate-stylelintrc', gulp.series(function (done) {
-	tsNode.register();
+gulp.task(
+	'generate-stylelintrc',
+	gulp.series(function (done) {
+		tsNode.register();
 
-	// Видалити кешовану версію модуля
-	delete require.cache[require.resolve('./src/index.ts')];
+		delete require.cache[require.resolve('./src/index.ts')];
 
-	const configModule = require('./src/index.ts');
-	const configContent = `module.exports = ${JSON.stringify(configModule.CONFIG, null, 2)};`;
+		const configModule = require('./src/index.ts');
+		const configContent = `module.exports = ${JSON.stringify(configModule.CONFIG, null, 2)};`;
 
-	fs.writeFileSync('.stylelintrc.js', configContent, 'utf-8');
+		fs.writeFileSync('.stylelintrc.js', configContent, 'utf-8');
 
-	done();
-}, 'prettify'));
+		done();
+	}, 'prettify')
+);
 
 gulp.task('watch', function () {
 	gulp.watch('src/**/*.ts', gulp.series('build'));

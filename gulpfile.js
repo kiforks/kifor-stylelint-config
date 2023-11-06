68c693914f2d0ce52cd293f81e9a6e4e1412a7db
Kifor Kostiantyn <ks.kifor@gmail.com>
2023-11-06 20:26:04 +0200

    Updated packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,26 @@
 const gulp = require('gulp');
 const fs = require('fs');
-const tsNode = require('ts-node');
-const prettier = require('gulp-prettier');
+
+require('ts-node').register();
 
 gulp.task('prettify', function () {
-	return gulp.src('.stylelintrc.js').pipe(prettier()).pipe(gulp.dest('.'));
+	return import('gulp-prettier').then(prettierModule => {
+		const prettier = prettierModule.default;
+		return gulp.src('.stylelintrc.js').pipe(prettier({})).pipe(gulp.dest('.'));
+	});
 });
 
-gulp.task(
-	'generate-stylelintrc',
-	gulp.series(function (done) {
-		tsNode.register();
-
-		delete require.cache[require.resolve('./src/index.ts')];
-
-		const configModule = require('./src/index.ts');
-		const configContent = `module.exports = ${JSON.stringify(configModule.CONFIG, null, 2)};`;
-
-		fs.writeFileSync('.stylelintrc.js', configContent, 'utf-8');
-
-		done();
-	}, 'prettify')
-);
+gulp.task('generate-stylelintrc', function (done) {
+	delete require.cache[require.resolve('./src/index.ts')];
+	const config = require('./src/index.ts');
+	const configContent = `module.exports = ${JSON.stringify(config.CONFIG, null, 2)};`;
+	fs.writeFileSync('.stylelintrc.js', configContent, 'utf8');
+	done();
+});
 
 gulp.task('watch', function () {
-	gulp.watch('src/**/*.ts', gulp.series('build'));
+	gulp.watch('src/**/*.ts', gulp.series('generate-stylelintrc', 'prettify'));
 });
 
-gulp.task('build', gulp.series('generate-stylelintrc'));
+gulp.task('build', gulp.series('generate-stylelintrc', 'prettify'));
 gulp.task('start', gulp.series('build', 'watch'));

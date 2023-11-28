e7ec57cfea637cd9d9a540434ad757310357c601
Kifor Kostiantyn <ks.kifor@gmail.com>
2023-11-28 21:40:15 +0200

    Changed configuration

--- src/index.ts
+++ src/index.ts
@@ -3,38 +3,53 @@ import { ORDER_PROPERTIES } from './core/modules/order-property/constants/order-
 import { RULE_PROPERTY_UNIT_ALLOWED_LIST } from './rule/configs/rule-property-unit-allowed-list/rule-property-unit-allowed-list.config';
 import { RULE_UNIT_ALLOWED_LIST } from './rule/configs/rule-unit-allowed-list/rule-unit-allowed-list.config';
 import { RULE_NO_UNKNOWN } from './rule/configs/rule-no-unknown/rule-no-unknown.config';
+import { Config } from 'stylelint';
 
-export const CONFIG = {
+export const CONFIG: Config = {
 	customSyntax: 'postcss-scss',
+	ignoreFiles: ['**/*.css'],
 	extends: ['stylelint-config-standard'],
 	plugins: ['stylelint-order'],
 	rules: {
-		'order/order': ORDER_CONTENT,
-		'order/properties-order': [
+		/* At-rule */
+		'at-rule-no-unknown': [
+			true,
 			{
-				properties: ORDER_PROPERTIES,
+				'ignoreAtRules': RULE_NO_UNKNOWN,
 			},
 		],
-		'declaration-block-no-duplicate-properties': true,
-		'declaration-property-unit-allowed-list': RULE_PROPERTY_UNIT_ALLOWED_LIST,
-		'selector-pseudo-element-no-unknown': [true, { ignorePseudoElements: ['ng-deep'] }],
-		'selector-not-notation': 'simple',
-		'declaration-block-no-redundant-longhand-properties': false,
 		'at-rule-property-required-list': {
 			'font-face': ['font-display', 'font-family', 'font-style'],
 		},
-		'color-no-hex': true,
+
+		/* Color */
 		'color-function-notation': ['legacy', { 'ignore': ['with-var-inside'] }],
+		'color-no-hex': true,
+
+		/* Declaration block */
+		'declaration-block-no-duplicate-properties': true,
+		'declaration-block-no-redundant-longhand-properties': false,
+
+		/* Declaration property */
+		'declaration-property-unit-allowed-list': RULE_PROPERTY_UNIT_ALLOWED_LIST,
+		'declaration-property-value-no-unknown': true,
+
+		/* Function */
 		'function-disallowed-list': ['rgb'],
 		'function-url-no-scheme-relative': true,
 		'function-url-scheme-disallowed-list': ['ftp', '/^http/'],
+
+		/* Media feature */
 		'media-feature-name-no-vendor-prefix': true,
 		'media-feature-name-unit-allowed-list': RULE_PROPERTY_UNIT_ALLOWED_LIST,
+		/* Rule */
 		'rule-selector-property-disallowed-list': {
 			'/ri\\-/': ['font-size'],
 			'/^\\.ri-/': ['font-size'],
 			'i': ['font-size'],
 		},
+
+		/* Selector */
 		'selector-disallowed-list': [
 			'i',
 			'/^\\.container/',
@@ -44,8 +59,30 @@ export const CONFIG = {
 			'/\\[data-test.+]/',
 			'/\\[data-po.+]/',
 		],
+		'selector-max-attribute': 1,
+		'selector-max-id': 1,
 		'selector-no-qualifying-type': true,
+		'selector-not-notation': 'simple',
+		'selector-pseudo-element-no-unknown': [true, { ignorePseudoElements: ['ng-deep'] }],
+
+		/* Time */
+		'time-min-milliseconds': 50,
+
+		/* Unit */
 		'unit-allowed-list': RULE_UNIT_ALLOWED_LIST,
+
+		/* Plugin */
+		'order/order': ORDER_CONTENT,
+		'order/properties-order': [
+			{
+				properties: ORDER_PROPERTIES,
+			},
+		],
+
+		/* Notation */
+		'font-weight-notation': 'numeric',
+
+		/* Other */
 		'max-nesting-depth': [
 			3,
 			{
@@ -54,16 +91,5 @@ export const CONFIG = {
 				'ignoreAtRules': ['/^\\include/', '/^\\media/'],
 			},
 		],
-		'at-rule-no-unknown': [
-			true,
-			{
-				'ignoreAtRules': RULE_NO_UNKNOWN,
-			},
-		],
-		'selector-max-attribute': 1,
-		'selector-max-id': 1,
-		'time-min-milliseconds': 50,
-		'font-weight-notation': 'numeric',
-		'declaration-property-value-no-unknown': true,
 	},
 };

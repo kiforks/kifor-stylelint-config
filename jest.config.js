54580fc88d571b407ef10d3c9564065c021cfdf2
Kifor Kostiantyn <ks.kifor@gmail.com>
2024-12-05 22:07:18 +0100

    feat [DEV-1]: Add paths to the jest configuration file

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,10 @@ export default {
 	},
 	testMatch: ['**/*.spec.ts', '**/*.spec.js'],
 	moduleFileExtensions: ['ts', 'js', 'json'],
+	moduleNameMapper: {
+		'^@core/(.*)$': '<rootDir>/src/core/$1',
+		'^@plugin/(.*)$': '<rootDir>/src/core/plugin/$1',
+		'^@rule/(.*)$': '<rootDir>/src/core/rule/$1',
+		'^@modules/(.*)$': '<rootDir>/src/core/modules/$1',
+	},
 };
